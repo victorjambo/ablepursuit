59b700e120f2727f249d9e6ae41048ba36bf0d05
victorjambo <victorjambo@live.com>
2017-09-03 00:49:17 +0300

    comment out

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,9 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   # change to pretty urls
 	# test title.parameterize in rails console to see how it works
-	#def to_param
-	#	"#{id}-#{title.parameterize}"		
-	#end
+
+	# old school pretty id
+	# def to_param
+	# 	"#{id}-#{title.parameterize}"		
+	# end
 end
