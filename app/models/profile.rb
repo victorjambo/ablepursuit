01ad4fe7370366129ea61bb91fac35a439e3a3b1
victorjambo <victorjambo@live.com>
2017-10-28 17:46:53 +0300

    [feature] stylings

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
 	belongs_to :user
 
 	# Paperclip
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.jpg"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 	# Impressionist
